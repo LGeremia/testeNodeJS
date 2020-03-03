1a04b0a1b21d7f6526c009cf5a4e94cc6d8b588e
Leonardo Geremia <leonardogeremisc@gmail.com>
2020-03-03 09:12:26 -0300

    Iniciando com modulos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var msg = require('./mod_teste');
 
 app.set('view engine', 'ejs');
 
@@ -16,5 +17,5 @@ app.get('/noticias', function(req, res){
 });
 
 app.listen(3000, function(){
-    console.log("servidor rodando com Express");
+    console.log(msg());
 });
---
+++ mod_teste.js
@@ -0,0 +1,4 @@
+module.exports = function(){
+    var msg = "Este modulo retorna uma mensagem apenas";
+    return msg;
+}
