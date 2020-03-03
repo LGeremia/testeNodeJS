870a11fb72b102f7ca4b55082d81905aabbb19bc
Leonardo Geremia <leonardogeremisc@gmail.com>
2020-03-03 09:02:52 -0300

    adicionando todas as views e o nodemon ao projeto

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.render("secao/tecnologia")
+    res.render("home/index")
+});
+
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render("admin/form_add_noticia")
+});
+
+app.get('/noticias', function(req, res){
+    res.render("noticias/noticias")
 });
 
 app.listen(3000, function(){
