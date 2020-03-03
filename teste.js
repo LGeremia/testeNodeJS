c9aa87424682f1dbec2fb7d42ce5cf465b79037f
Leonardo Geremia <leonardogeremisc@gmail.com>
2020-03-03 08:14:54 -0300

    interpretando request

--- teste.js
+++ teste.js
@@ -1,5 +1,14 @@
 var http = require('http');
 var server = http.createServer(function(req,res){
-    res.end("<html><body> Portal de Notícias</body></html>")
+    var categoria = req.url;
+    if(categoria == '/tecnologia'){
+        res.end("<html><body> Notícias Tecnologia</body></html>")
+    }else if(categoria == '/Moda'){
+        res.end("<html><body> Notícias Moda</body></html>")
+    }else if(categoria == '/Teste'){
+        res.end("<html><body> Notícias Teste</body></html>")
+    }else{
+        res.end("<html><body> Portal de Notícias</body></html>")
+    }
 });
 server.listen(3000);
